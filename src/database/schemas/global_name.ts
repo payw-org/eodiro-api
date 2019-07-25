ed1773ae6fc2d866d90361f4bab668d4ad7e9b3c
ihooni <to@ihooni.com>
2019-07-25 14:33:55 +0900

    Correct language code. ('ko' -> 'kr')

--- src/database/schemas/global_name.ts
+++ src/database/schemas/global_name.ts
@@ -9,14 +9,14 @@ import mongoose, { Document } from 'mongoose'
 
 export interface GlobalNameDoc extends Document {
   [index: string]: string | any
-  ko: string
+  kr: string
   en: string
   zh?: string
   fr?: string
 }
 
 const globalNameSchema = new mongoose.Schema({
-  ko: { type: String, required: true },
+  kr: { type: String, required: true },
   en: { type: String, required: true },
   zh: { type: String },
   fr: { type: String }
