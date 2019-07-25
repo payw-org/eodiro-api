52ff82b56b5bfd577f558de93f91fb6e0861b1d7
ihooni <to@ihooni.com>
2019-07-25 18:22:41 +0900

    Create campuses controller.

---
+++ src/app/http/controllers/CampusesController.ts
@@ -0,0 +1,55 @@
+import { SimpleHandler } from 'Http/RequestHandler'
+import University from 'Database/models/university'
+import logger from 'Configs/log'
+import { UniversityDoc } from 'Database/schemas/university'
+
+interface CampusDesc {
+  university: string
+  campus: string
+  vendor: string
+}
+
+export default class CampusesController {
+  /**
+   * Display a listing of the campus.
+   */
+  public static index(): SimpleHandler {
+    return async (req, res) => {
+      let language = req.body.language
+
+      // find all university document
+      const universities = <UniversityDoc[]>await University.find(
+        {},
+        { _id: 0, name: 1, campus: 1, vendor: 1 },
+        err => {
+          if (err) {
+            logger.error(err)
+          }
+        }
+      ).sort([['name.' + language, 1]])
+
+      // if not exist
+      if (universities.length === 0) {
+        res.status(404).json({
+          err: {
+            msg: 'Campuses are not found'
+          }
+        })
+      }
+
+      // data formatting
+      const campus_list: CampusDesc[] = []
+      universities.forEach(univ => {
+        campus_list.push({
+          university: univ.name[language],
+          campus: univ.campus[language],
+          vendor: univ.vendor
+        })
+      })
+
+      res.status(200).json({
+        campuses: campus_list
+      })
+    }
+  }
+}
