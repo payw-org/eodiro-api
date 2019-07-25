2a8e902ebfd3b13bf6d5fcf5fa21ea73fa42be6b
ihooni <to@ihooni.com>
2019-07-25 22:04:39 +0900

    Create middleware for vendor checking.

---
+++ src/app/http/middleware/CheckVendor.ts
@@ -0,0 +1,32 @@
+import { NextHandler } from 'Http/RequestHandler'
+import University from 'Database/models/university'
+import logger from 'Configs/log'
+
+export default class CheckVendor {
+  /**
+   * Check if vendor is not exist.
+   */
+  public static handler(): NextHandler {
+    return async (req, res, next) => {
+      let vendor = req.params.vendor
+
+      // find(count) vendor
+      let count = await University.count({ vendor: vendor }, err => {
+        if (err) {
+          logger.error(err)
+        }
+      })
+
+      // if not exist
+      if (count === 0) {
+        res.status(404).json({
+          err: {
+            msg: 'Vendor not found'
+          }
+        })
+      }
+
+      return next()
+    }
+  }
+}
