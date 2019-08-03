89dd372f541064d1b523db15ab7b90b7b7bca1c6
ihooni <to@ihooni.com>
2019-08-03 21:13:49 +0900

    Create middleware for building check
    # Apply middleware to all sub endpoint of `/buildings/:building`

--- src/routes/vacant.ts
+++ src/routes/vacant.ts
@@ -1,8 +1,17 @@
 import express from 'express'
 import BuildingsController from 'Http/controllers/vacant/BuildingsController'
+import CheckBuildingMiddleware from 'Http/middleware/CheckBuilding'
 
 const router = express.Router()
 
+/**
+ * Middleware
+ */
+router.use('/buildings/:building', CheckBuildingMiddleware.handler())
+
+/**
+ * Controller
+ */
 // show building list
 router.get('/buildings', BuildingsController.index())
 
