0ebe3e2b08e7076e0b51b809c9c4697b5b17906c
ihooni <to@ihooni.com>
2019-08-04 21:12:45 +0900

    Create middleware for floor check
    # Apply middleware to all sub endpoint of `/buildings/:building/floors/:floor`

--- src/routes/vacant.ts
+++ src/routes/vacant.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import BuildingsController from 'Http/controllers/vacant/BuildingsController'
 import CheckBuildingMiddleware from 'Http/middleware/CheckBuilding'
 import FloorsController from 'Http/controllers/vacant/FloorsController'
+import CheckFloorMiddleware from 'Http/middleware/CheckFloor'
 
 const router = express.Router()
 
@@ -9,6 +10,7 @@ const router = express.Router()
  * Middleware
  */
 router.use('/buildings/:building', CheckBuildingMiddleware.handler())
+router.use('/buildings/:building/floors/:floor', CheckFloorMiddleware.handler())
 
 /**
  * Controller
