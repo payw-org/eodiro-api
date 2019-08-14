05ac6b92da1a4ba4d41c708b823ff11c737d222c
H.Chihoon <to@ihooni.com>
2019-08-14 14:26:17 +0900

    Create database seeder.

---
+++ src/app/db/DBSeeder.ts
@@ -0,0 +1,181 @@
+import { GlobalNameDoc } from 'Database/schemas/global_name'
+import metadata_seoul_json from 'Resources/metadata/cau_seoul.json'
+import metadata_anseong_json from 'Resources/metadata/cau_anseong.json'
+import classes_seoul_json from 'Resources/classes/cau_seoul.json'
+import classes_anseong_json from 'Resources/classes/cau_anseong.json'
+import University from 'Database/models/university'
+import Building from 'Database/models/building'
+import Class from 'Database/models/class'
+import logger from 'Configs/log'
+import { ClassDoc } from 'Database/schemas/class'
+import Floor from 'Database/models/floor'
+import Classroom from 'Database/models/classroom'
+import Lecture from 'Database/models/lecture'
+import { UniversityDoc } from 'Database/schemas/university'
+
+interface ClassesJSON {
+  vendor: string
+  classes: ClassDoc[]
+}
+
+interface BldgMetaJSON {
+  number: string
+  name: string
+}
+
+interface UnivMetaJSON {
+  name: GlobalNameDoc
+  campus: GlobalNameDoc
+  vendor: string
+  buildings: BldgMetaJSON[]
+}
+
+export default class DBSeeder {
+  /**
+   * Seed(resource) data for metadata
+   */
+  private metadata_seed: UnivMetaJSON[] = [
+    <UnivMetaJSON>metadata_seoul_json,
+    <UnivMetaJSON>metadata_anseong_json
+  ]
+
+  /**
+   * Seed(resource) data for classes
+   */
+  private classes_seed: ClassesJSON[] = [
+    <ClassesJSON>classes_seoul_json,
+    <ClassesJSON>classes_anseong_json
+  ]
+
+  /**
+   * Seed the collection of university and building.
+   */
+  public async seedMetadata(): Promise<void> {
+    // asynchronously seed metadata
+    await Promise.all(
+      this.metadata_seed.map(async (seed: UnivMetaJSON) => {
+        // create university
+        const university = await University.create({
+          name: seed.name,
+          campus: seed.campus,
+          vendor: seed.vendor
+        })
+
+        // create buildings
+        const buildings = await Building.insertMany(seed.buildings)
+        // get building id list
+        let buildings_ids = buildings.map(bldg => {
+          return bldg._id
+        })
+
+        // link university to buildings
+        await Building.updateMany(
+          { _id: { $in: buildings_ids } },
+          { university: university._id }
+        )
+
+        // link buildings to university
+        await University.findByIdAndUpdate(university._id, {
+          buildings: buildings
+        })
+      })
+    )
+  }
+
+  /**
+   * Seed the collection of class, floor, classroom, lecture.
+   */
+  public async seedClasses(): Promise<void> {
+    // asynchronously seed classes
+    await Promise.all(
+      this.classes_seed.map(async (seed: ClassesJSON) => {
+        try {
+          // create classes
+          const classes = <ClassDoc[]>await Class.insertMany(seed.classes)
+
+          // link classes to university
+          const university = <UniversityDoc>(
+            await University.findOneAndUpdate(
+              { vendor: seed.vendor },
+              { classes: classes },
+              { new: true }
+            )
+          )
+        } catch (err) {
+          logger.error('Class save error: ' + err)
+        }
+      })
+    )
+
+    const classes = <ClassDoc[]>await Class.find()
+    // create floors, classrooms, lectures
+    for (const cls of classes) {
+      for (let i = 0; i < cls.locations.length; i++) {
+        let location = cls.locations[i]
+        let floor_num = location.room.match(/(?:[Bb]|)\d+(?=\d\d)/)[0]
+
+        // find class building
+        const building = await Building.findOne({
+          number: location.building
+        })
+
+        // find class floor
+        // if not exist, create floor
+        const floor = await Floor.findOneAndUpdate(
+          { building: building._id, number: floor_num },
+          { building: building._id, number: floor_num },
+          { upsert: true, new: true }
+        )
+
+        // link floor to building
+        await Building.findByIdAndUpdate(building._id, {
+          $addToSet: { floors: floor._id }
+        })
+
+        // find classroom
+        // if not exist, create classroom
+        const classroom = await Classroom.findOneAndUpdate(
+          { floor: floor._id, number: location.room },
+          { floor: floor._id, number: location.room },
+          { upsert: true, new: true }
+        )
+
+        // link classroom to floor
+        await Floor.findByIdAndUpdate(floor._id, {
+          $addToSet: { classrooms: classroom._id }
+        })
+
+        // create lecture
+        const lecture = await Lecture.create({
+          classroom: classroom._id,
+          class: cls._id,
+          order: i
+        })
+
+        // link lecture to classroom
+        await Classroom.findByIdAndUpdate(classroom._id, {
+          $addToSet: { lectures: lecture._id }
+        })
+      }
+    }
+  }
+
+  /**
+   * Apply refreshed seed data.
+   */
+  public async refreshMetadataAndClasses(): Promise<void> {
+    // delete all documents about metadata and classes
+    await Promise.all([
+      University.deleteMany({}),
+      Building.deleteMany({}),
+      Class.deleteMany({}),
+      Floor.deleteMany({}),
+      Classroom.deleteMany({}),
+      Lecture.deleteMany({})
+    ])
+
+    // re-seeding
+    await this.seedMetadata()
+    await this.seedClasses()
+  }
+}
