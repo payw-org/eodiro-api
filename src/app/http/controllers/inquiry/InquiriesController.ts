98793bc29503e73b16af2b43c0794d9d14b6d2f2
H.Chihoon <to@ihooni.com>
2019-08-20 21:30:17 +0900

    Make inquiry feature.
    # Create controller for inquiry
    # Create and set route.
    # Update `package.json`

---
+++ src/app/http/controllers/inquiry/InquiriesController.ts
@@ -0,0 +1,62 @@
+import { SimpleHandler, Response } from 'Http/RequestHandler'
+import {
+  checkSchema,
+  ValidationChain,
+  validationResult
+} from 'express-validator'
+import Mailer from 'Helpers/Mailer'
+import University from 'Database/models/university'
+import { UniversityDoc } from 'Database/schemas/university'
+import uniqid from 'uniqid'
+
+export default class InquiriesController {
+  /**
+   * Validate `create` request.
+   */
+  public static validateCreate(): ValidationChain[] {
+    return checkSchema({
+      text: {
+        exists: true,
+        in: 'body',
+        isString: true,
+        isLength: { options: { min: 2, max: 500 } },
+        trim: true,
+        escape: true,
+        errorMessage: 'Text must be 2 - 500 length string.'
+      }
+    })
+  }
+
+  /**
+   * Create a new inquiry.
+   * Send inquiry to eodiro mailbox.
+   */
+  public static create(): SimpleHandler {
+    return async (req, res): Promise<Response> => {
+      // Handle validation errors
+      const errors = validationResult(req)
+      if (!errors.isEmpty()) {
+        return res
+          .status(422)
+          .json({ errors: errors.array({ onlyFirstError: true }) })
+      }
+
+      // get current vendor name
+      const university = (await University.findById(res.locals.univId, {
+        _id: 0,
+        vendor: 1
+      })) as UniversityDoc
+
+      // send mail
+      Mailer.sendMail({
+        from: '"어디로" <contact@payw.org>',
+        to: 'contact@payw.org',
+        subject:
+          '어디로 문의 <' + university.vendor + '> - ' + uniqid.process(),
+        text: req.body.text
+      })
+
+      return res.status(204).json({})
+    }
+  }
+}
--- src/routes/api.ts
+++ src/routes/api.ts
@@ -3,7 +3,7 @@ import LocaleMiddleware from 'Http/middleware/Locale'
 import CheckVendorMiddleware from 'Http/middleware/CheckVendor'
 import CampusesController from 'Http/controllers/CampusesController'
 import vacantRouter from 'Routes/vacant'
-import emptyCount from 'Database/models/empty_count'
+import inquiryRouter from 'Routes/inquiry'
 
 const router = express.Router()
 
@@ -16,17 +16,12 @@ router.use('/campuses/:vendor', CheckVendorMiddleware.handler())
  * Sub router
  */
 router.use('/campuses/:vendor/vacant', vacantRouter)
+router.use('/campuses/:vendor/inquiry', inquiryRouter)
 
 /**
  * Controller
  */
 // show campus list
 router.get('/campuses', LocaleMiddleware.handler(), CampusesController.index())
-
-// router.get('/test', (req, res) => {
-//   res.json({
-//     result: 'test'
-//   })
-// })
 
 export default router
