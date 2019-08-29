ded761b14114f48c5854ae758d0097dce91b2720
H.Chihoon <to@ihooni.com>
2019-08-29 20:30:53 +0900

    Bug fix
    # Before that, server error handler cannot to handle error.

--- src/app/http/middleware/HandleServerError.ts
+++ src/app/http/middleware/HandleServerError.ts
@@ -4,9 +4,10 @@ import LogHelper from 'Helpers/LogHelper'
 export default class HandleServerError {
   /**
    * Handle 500 http error.
+   * This is last error handler.
    */
   public static handler(): ErrorHandler {
-    return (err, req, res): Response => {
+    return (err, req, res, next): Response => {
       LogHelper.log('error', err.stack)
 
       return res.status(500).json({
