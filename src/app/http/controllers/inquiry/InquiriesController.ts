6edd6abf208d8073caccee56047a845cf82351c7
H.Chihoon <to@ihooni.com>
2019-09-03 19:20:15 +0900

    Improve inquiry feature.
    # Add reply email address
    # Use `parseInt` instead of `Number`

--- src/app/http/controllers/inquiry/InquiriesController.ts
+++ src/app/http/controllers/inquiry/InquiriesController.ts
@@ -17,6 +17,13 @@ export default class InquiriesController {
         trim: true,
         escape: true,
         errorMessage: '`text` must be 2 - 500 length string.'
+      },
+      email: {
+        optional: true,
+        in: 'body',
+        isEmail: true,
+        trim: true,
+        errorMessage: '`email` must be follow email format.'
       }
     })
   }
@@ -27,12 +34,20 @@ export default class InquiriesController {
    */
   public static create(): SimpleHandler {
     return (req, res): Response => {
+      let mailText = req.body.text
+
+      if (req.body.email) {
+        mailText += '\n\n답변주소: ' + req.body.email
+      } else {
+        mailText += '\n\n답변주소 미지정'
+      }
+
       // send mail
       Mailer.sendMail({
         from: '"어디로" <contact@payw.org>',
         to: 'contact@payw.org',
         subject: '어디로 문의 [' + req.params.vendor + '] - ' + nanoid(10),
-        text: req.body.text
+        text: mailText
       })
 
       return res.status(204).json({})
