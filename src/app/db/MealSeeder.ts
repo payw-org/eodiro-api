87ec4edf41ccb6a41d4019102bc6701ebaa64fd1
H.Chihoon <to@ihooni.com>
2019-09-07 03:07:09 +0900

    Update `MealSeeder`
    # Handle the case that meals are empty
    # Calculate day difference by reflecting timezone offset
    # Add comments

--- src/app/db/MealSeeder.ts
+++ src/app/db/MealSeeder.ts
@@ -6,25 +6,49 @@ import { DayMealDoc } from 'Database/schemas/day-meal'
 const dayScope = 5
 
 export default class MealSeeder {
+  /**
+   * Seed(resource) data for meals
+   */
   private mealsSeed: Data
 
+  /**
+   * Scrape meals and seed it to database.
+   */
   public async run(): Promise<void> {
+    // get most recent meal
     const recentMeal = (await DayMeal.findOne({}, { _id: 0, date: 1 }).sort(
       '-date'
     )) as DayMealDoc
 
+    // if meals are empty
+    if (recentMeal === null) {
+      await this.scrapeMeals()
+      await this.seedMeals()
+
+      return
+    }
+
     const recentDate = new Date(recentMeal.date)
     const currentDate = new Date()
+
+    // calculate day difference between recent meal date and current date
     const dayDiff = Math.ceil(
-      (recentDate.getTime() - currentDate.getTime()) / (1000 * 60 * 60 * 24)
+      (recentDate.getTime() +
+        currentDate.getTimezoneOffset() * 1000 * 60 -
+        currentDate.getTime()) /
+        (1000 * 60 * 60 * 24)
     )
 
+    // if need to get new meals
     if (dayDiff < dayScope - 1) {
       await this.scrapeMeals()
       await this.seedMeals()
     }
   }
 
+  /**
+   * Scrape meal list from website
+   */
   private async scrapeMeals(): Promise<void> {
     this.mealsSeed = await CAUFoodScraper({
       id: process.env.FOOD_SCRAPER_ID,
@@ -33,11 +57,14 @@ export default class MealSeeder {
     })
   }
 
+  /**
+   * Seed scraped meals to database.
+   */
   private async seedMeals(): Promise<void> {
-    await DayMeal.deleteMany({})
+    await DayMeal.deleteMany({}) // delete old meals
 
     try {
-      await DayMeal.insertMany(this.mealsSeed)
+      await DayMeal.insertMany(this.mealsSeed) // seed new
     } catch (err) {
       LogHelper.log('error', 'Meal save error: ' + err)
     }
