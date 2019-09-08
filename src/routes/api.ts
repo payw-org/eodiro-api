55d8b1b63859df7d4fbe33348edfffb316d6c1d3
H.Chihoon <to@ihooni.com>
2019-09-09 02:54:31 +0900

    Create router for meal feature and add it to main router

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -5,6 +5,7 @@ import CampusesController from 'Http/controllers/CampusesController'
 import vacantRouter from 'Routes/vacant'
 import inquiryRouter from 'Routes/inquiry'
 import searchClassRouter from 'Routes/./search-class'
+import mealRouter from 'Routes/meal'
 
 const router = express.Router()
 
@@ -19,6 +20,7 @@ router.use('/campuses/:vendor', CheckVendorMiddleware.handler())
 router.use('/campuses/:vendor/vacant', vacantRouter)
 router.use('/campuses/:vendor/inquiry', inquiryRouter)
 router.use('/campuses/:vendor/search-class', searchClassRouter)
+router.use('/campuses/:vendor/meal', mealRouter)
 
 /**
  * Controller
